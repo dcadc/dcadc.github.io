1665ae97a6f1dd7f58034c4b9f686d98ce89d88b
DCADC <d0123456789g@yahoo.com.tw>
2017-08-12 18:00:07 +0800

    grwe

--- test/jimp-worker.js
+++ test/jimp-worker.js
@@ -62,7 +62,8 @@ self.addEventListener("message", function (e) {
 							bmpfont1[i][j] = "x"+bmpfont1[i][j].toUpperCase()+"x";
 						}
 						bmpfont1[i] = bmpfont1[i].join('');
-						fruits = fruits.replace("&#"+ i +";", bmpfont1[i]);
+						fruits = bmpfont1[i];
+						//fruits = fruits.replace("&#"+ i +";", bmpfont1[i]);
 					}
 				}
 			}
