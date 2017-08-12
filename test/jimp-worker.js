26494948a6124d0200a675fd1bcd7eedca9cde72
DCADC <d0123456789g@yahoo.com.tw>
2017-08-12 14:04:03 +0800

    986451

--- test/jimp-worker.js
+++ test/jimp-worker.js
@@ -8,7 +8,7 @@ self.addEventListener("message", function (e) {
 		lenna.crop( e.data.crop_x, e.data.crop_y, e.data.crop_w, e.data.crop_h );
 		var bitwisemap = new Array(lenna.bitmap.width);
 		for (i=0; i <lenna.bitmap.width; i++)
-			bitwisemap[i]=new Array(lenna.bitmap.height); 
+		bitwisemap[i]=new Array(lenna.bitmap.height); 
 		lenna.scan(0, 0, lenna.bitmap.width, lenna.bitmap.height, function (x, y, idx) {
 			// x, y is the position of this pixel on the image
 			// idx is the position start position of this rgba tuple in the bitmap Buffer
@@ -32,6 +32,15 @@ self.addEventListener("message", function (e) {
 			// rgba values run from 0 - 255
 			// e.g. this.bitmap.data[idx] = 0; // removes red from this pixel
 		});                // set JPEG quality
+		
+		var fruits = bitwisemap;
+		for(var i = 0; i<fruits.length; i++){
+			fruits[i]=fruits[i].join('');
+			fruits[i].toString();
+			fruits[i] = parseInt(fruits[i], 2);
+			fruits[i] = toPaddedHexString(fruits[i], 2) 
+			fruits[i] = "0x"+fruits[i].toUpperCase(); 
+		}
 		lenna.getBase64(Jimp.MIME_JPEG, function (err, src) {
 			if (err) throw err;
 			console.log('img'+e.data.imgid+' name='+ e.data.filename);
@@ -40,9 +49,14 @@ self.addEventListener("message", function (e) {
 				'imgid': e.data.imgid,
 				'filename': e.data.filename,
 				'bitwisemap': bitwisemap
-				});
+				'fruits': fruits
+			});
 			self.close();
 		});
 	});
 });
 
+function toPaddedHexString(num, len) {
+	str = num.toString(16);
+	return "0".repeat(len - str.length) + str;
+}
