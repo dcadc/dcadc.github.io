82eee6dc1057f056f6a28c04b3ffc4d028fdb889
DCADC <d0123456789g@yahoo.com.tw>
2017-08-13 13:32:40 +0800

    4864

--- test/jimp-worker.js
+++ test/jimp-worker.js
@@ -41,7 +41,7 @@ self.addEventListener("message", function (e) {
 						this.bitmap.data[ idx + i ] = 255;
 					}
 				}
-				console.log('pixel ('+x+', '+y+')'+' content= '+lenna.getPixelColor(x, y)+'  '+parseInt(this.bitmap.data));
+				console.log('pixel ('+x+', '+y+')'+' idx '+idx+' content= '+lenna.getPixelColor(x, y)+'  '+Jimp.rgbaToInt(red, green, blue, alpha));
 				
 				bitwisemap[regno][x][y] = ( this.bitmap.data[ idx ] == 255 ) ? (1) : (0) ;
 				//document.writeln(green);
