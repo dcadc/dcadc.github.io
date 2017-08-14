67c9df1cedf4a50337388dacd8882f67d68b7d92
DCADC <d0123456789g@yahoo.com.tw>
2017-08-14 19:41:10 +0800

    cvdsc

--- test/jimp-worker.js
+++ test/jimp-worker.js
@@ -8,7 +8,7 @@ self.addEventListener("message", function (e) {
 	Jimp.read(e.data.cmd).then(function (lenna) {
 		var lenna_bkp = lenna.clone();
 		var monkey = new Array(e.data.region_data.length);
-		var font = new Array(e.data.region_data.length);
+		var font = JSON.parse(JSON.stringify(bmpfont1));
 		for(var region_id = 0; region_id < e.data.region_data.length; region_id++){
 			lenna = lenna_bkp.clone();
 for (var i = 0; i < 5; i++)	console.log('e.data.region_data[region_id][i]' + typeof e.data.region_data[region_id][i] + 'val' + e.data.region_data[region_id][i]);
@@ -47,45 +47,20 @@ for (var i = 0; i < 5; i++)	console.log('e.data.region_data[region_id][i]' + typ
 			});
 			
 			monkey[region_id].extracted = monkey[region_id].map_of_bits.slice(0);
-			font[region_id] = JSON.parse(JSON.stringify(bmpfont1));
+			font = JSON.parse(JSON.stringify(bmpfont1));
 			
-			if ( e.data.region_data[region_id][3] > font[region_id].height) {
-				for(var i = 0; i < monkey[region_id].extracted.length; i++){								//for each columns
-					monkey[region_id].extracted[i]=monkey[region_id].extracted[i].join('');					//join the binary value of columns
-					monkey[region_id].extracted[i] = parseInt(monkey[region_id].extracted[i], 2);			//prase the joined binary columns into decimal int
-					monkey[region_id].extracted[i] = toPaddedHexString(monkey[region_id].extracted[i], 2);	//prase the decimal int to HEX with padding zeros
-					monkey[region_id].extracted[i] = "x"+monkey[region_id].extracted[i].toUpperCase()+"x"; 	//wrap the uppercased HEX column data with x & x
-				}
-				
-				monkey[region_id].extracted = monkey[region_id].extracted.join('');							//join all the HEX columns into string
+			if ( e.data.region_data[region_id][3] == font.height) {
+
+				monkey[region_id].extracted = calculateColumn(monkey[region_id].extracted);
 				monkey[region_id].original = monkey[region_id].extracted.slice(0);							//clone the undecoded string 
 				
-				for(var i = font[region_id].data.length-1; i >= 0; i--){
-					if((font[region_id].data[i]) != null){
-						if((font[region_id].data[i].length) > 0){
-//console.log('i:'+i+' font[region_id].data[i]:'+font[region_id].data[i]);
-							for(var j = 0; j < font[region_id].data[i].length; j++){
-								font[region_id].data[i][j] = toPaddedHexString(font[region_id].data[i][j], 2);
-								font[region_id].data[i][j] = "x"+font[region_id].data[i][j].toUpperCase()+"x";
-							}
-							font[region_id].data[i] = font[region_id].data[i].join('');
-							monkey[region_id].extracted = monkey[region_id].extracted.replace(new RegExp(font[region_id].data[i], 'g'), "&#"+ i +";");
-						}
-					}
-				}
+				monkey[region_id].extracted = SearchandReplace(monkey[region_id].extracted, font.data);
 			}
-			else if ( e.data.region_data[region_id][3] == font[region_id].height) {
-				for(var i = 0; i < monkey[region_id].extracted.length; i++){								//for each columns
-					monkey[region_id].extracted[i]=monkey[region_id].extracted[i].join('');					//join the binary value of columns
-					monkey[region_id].extracted[i] = parseInt(monkey[region_id].extracted[i], 2);			//prase the joined binary columns into decimal int
-					monkey[region_id].extracted[i] = toPaddedHexString(monkey[region_id].extracted[i], 2);	//prase the decimal int to HEX with padding zeros
-					monkey[region_id].extracted[i] = "x"+monkey[region_id].extracted[i].toUpperCase()+"x"; 	//wrap the uppercased HEX column data with x & x
-				}
-				
-				monkey[region_id].extracted = monkey[region_id].extracted.join('');							//join all the HEX columns into string
+			else if ( e.data.region_data[region_id][3] > font.height) {
+				monkey[region_id].extracted = calculateColumn(monkey[region_id].extracted);
 				monkey[region_id].original = monkey[region_id].extracted.slice(0);							//clone the undecoded string 
 				
-				monkey[region_id].extracted = SearchandReplace(monkey[region_id].extracted, font[region_id].data);
+				monkey[region_id].extracted = SearchandReplace(monkey[region_id].extracted, font.data);
 				
 			}
 			else{
@@ -102,7 +77,7 @@ for (var i = 0; i < 5; i++)	console.log('e.data.region_data[region_id][i]' + typ
 			monkey[region_id].number_of.valid_chars = (monkey[region_id].extracted.match(new RegExp("([&#]{2})(4[89]|5[0-7]|6[5-9]|[78][0-9]|90|9[7-9]|1[01][0-9]|12[0-2])([;]{1})", "g")) || []).length;
 			
 console.log('img:'+e.data.imgid+' name:'+ e.data.filename+' region:'+region_id+ ' cropped.x:'+monkey[region_id].map_of_bits.length+ ' cropped.y:'+monkey[region_id].map_of_bits[0].length);
-console.log(' font:'+font[region_id]+' chars:'+monkey[region_id].number_of.chars+' valid chars:'+monkey[region_id].number_of.valid_chars);
+console.log(' font:'+font+' chars:'+monkey[region_id].number_of.chars+' valid chars:'+monkey[region_id].number_of.valid_chars);
 		}
 		self.postMessage({
 			'imgid': e.data.imgid,
@@ -118,16 +93,26 @@ function toPaddedHexString(num, len) {
 	return "0".repeat(len - str.length) + str;
 }
 
+function calculateColumn(map) {
+	for(var i = 0; i < map.length; i++){		//for each columns
+		map[i] = map[i].join('');				//join the binary value of columns
+		map[i] = parseInt(map[i], 2);			//prase the joined binary columns into decimal int
+		map[i] = toPaddedHexString(map[i], 2);	//prase the decimal int to HEX with padding zeros
+		map[i] = "x"+map[i].toUpperCase()+"x"; 	//wrap the uppercased HEX column data with x & x
+	}
+	return map.join('');						//join all the HEX columns into string
+}
+
 function SearchandReplace(col, fon) {
-	for(var i = fon.length-1; i >= 0; i--){
-		if((fon[i]) != null){
-			if((fon[i].length) > 0){
+	for(var i = fon.length-1; i >= 0; i--){							//for each symbol
+		if((fon[i]) != null){										//bypass undefined(i.e. control symbols)
+			if((fon[i].length) > 0){								//bypass unknown fonts
 				//console.log('i:'+i+' fon[i]:'+fon[i]);
-				for(var j = 0; j < fon[i].length; j++){
-					fon[i][j] = toPaddedHexString(fon[i][j], 2);
-					fon[i][j] = "x"+fon[i][j].toUpperCase()+"x";
+				for(var j = 0; j < fon[i].length; j++){				//matching the payload
+					fon[i][j] = toPaddedHexString(fon[i][j], 2);	//prase the decimal int to HEX with padding zeros
+					fon[i][j] = "x"+fon[i][j].toUpperCase()+"x"; 	//wrap the uppercased HEX column data with x & x
 				}
-				fon[i] = fon[i].join('');
+				fon[i] = fon[i].join('');							//font is in string, ready to be replaced
 				col = col.replace(new RegExp(fon[i], 'g'), "&#"+ i +";");
 			}
 		}
